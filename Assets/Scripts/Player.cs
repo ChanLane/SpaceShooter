3b27766e8c8467042981578048b8e8e3a894ea05
Chandler Lane <chandler.script@gmail.com>
2022-06-10 16:15:16 -0400

    implement player bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,17 +8,39 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float movementSpeed;
     // Start is called before the first frame update
+
+    [FoldoutGroup("Player Bounds")]
+    [SerializeField] private float xNegBound;
+    
+    [FoldoutGroup("Player Bounds")]
+    [SerializeField] private float xPosBound;
+
+    [FoldoutGroup("Player Bounds")]
+    [SerializeField] private float yNegBound;
+    
+    [FoldoutGroup("Player Bounds")]
+    [SerializeField] private float yPosBound;
+
+    #region Unity
     void Start()
     {
         transform.position = Vector3.zero;
     }
-
-    // Update is called once per frame
+    
     void Update()
-    {
-        transform.Translate(GetMovementDirection() * (movementSpeed * Time.deltaTime));
+    { 
+        HandleMovement();
     }
 
+    #endregion
+
+    #region CustomMethods
+
+    private void HandleMovement()
+    {
+        transform.Translate(GetMovementDirection() * (movementSpeed * Time.deltaTime));
+        EnforcePlayerBounds();
+    }
     private static Vector3 GetMovementDirection()
     {
         var horizontalInput = Input.GetAxis("Horizontal");
@@ -26,4 +49,33 @@ public class Player : MonoBehaviour
 
         return direction;
     }
+    private void EnforcePlayerBounds()
+    {
+        var position = transform.position;
+        var yValue = position.y;
+        var xValue = position.x;
+        
+        if (transform.position.x < xNegBound)
+        {
+            transform.position = new Vector3(xPosBound, yValue, 0);
+        }
+
+        if (transform.position.x > xPosBound)
+        {
+            transform.position = new Vector3(xNegBound, yValue, 0);
+        }
+
+        if (transform.position.y <= yNegBound) 
+        {
+            transform.position = new Vector3(xValue, yNegBound, 0);
+        }
+
+        if (transform.position.y >= yPosBound)
+        {
+            transform.position = new Vector3(xValue, yPosBound, 0);
+        }
+    }
+
+    #endregion
+    
 }
